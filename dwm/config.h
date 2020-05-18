8c65a77150db1013d5be1eff3711461de21e0da0
Shubham Saini <shubham6405@gmail.com>
2020-05-18 20:25:32 +0530

    finally done

--- dwm/config.h
+++ dwm/config.h
@@ -3,30 +3,23 @@
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 8;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 8;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 8;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 8;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hermit:pixelsize=11:antialias=true:autohint=true" };
+static const char *fonts[]          = { "Hermit:pixelsize=12.5:antialias=true:autohint=true" };
 static const char col_gray1[]       = "#696969";
 static const char col_gray2[]       = "#000000";
 static const char col_gray3[]       = "#c6c8d1";
 static const char col_gray4[]       = "#696969";
 static const char col_cyan[]        = "#e0e0e0";
-static const char col_black[]       = "#000000";
-static const char col_red[]         = "#ff0000";
-static const char col_yellow[]      = "#ffff00";
-static const char col_white[]       = "#ffffff";
-#define NUMCOLORS         4
 static const char *colors[][3]      = {
 	/*					fg         bg          border   */
 	[SchemeNorm] =	 { col_gray4, col_gray2,  col_gray2 },
-	[SchemeSel]  =	 { col_gray3, col_gray2,   col_gray2 },
-	[SchemeWarn] =	 { col_black, col_yellow, col_red },
-	[SchemeUrgent]=	 { col_white, col_red,    col_red },
+	[SchemeSel]  =	 { col_gray3, col_gray2,   col_gray2},
 };
 
 /* tagging */
@@ -45,7 +38,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -70,7 +63,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "menu", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "80x24", NULL };
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 
 /* volume */
 static const char *upvol[]   = { "pulsemixer", "--change-volume", "+5",  NULL };
@@ -81,11 +74,6 @@ static const char *shut[] = { "shut",  NULL };
 /* brightness */
 static const char *brightup[]   = { "doas", "bup",  NULL };
 static const char *brightdown[]   = { "doas", "bdown",  NULL };
-
-/* cmus */
-static const char *plyrnext[]   = { "cmus-remote", "-n",  NULL };
-static const char *plyrprev[]   = { "cmus-remote", "-r",  NULL };
-static const char *plyrplay[]   = { "cmus-remote", "-u",  NULL };
 
 /* lock */
 static const char *lock[]   = { "xset", "s", "activate",  NULL };
@@ -101,9 +89,6 @@ static Key keys[] = {
 	{ 0,                            XF86XK_AudioRaiseVolume, spawn,          {.v = upvol   } },
 	{ 0,                            XF86XK_MonBrightnessUp, spawn,          {.v = brightup } },
 	{ 0,                            XF86XK_MonBrightnessDown, spawn,          {.v = brightdown  } },
-	{ 0,                            XF86XK_AudioNext,        spawn, {.v = plyrnext } },
-	{ 0,                            XF86XK_AudioPrev,        spawn, {.v = plyrprev } },
-	{ 0,                            XF86XK_AudioPlay,        spawn, {.v = plyrplay } },
 	{ MODKEY|ShiftMask,             XK_Delete,  spawn,         {.v = lock } }, 
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -112,14 +97,14 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|Mod1Mask,              XK_h,      incrgaps,       {.i = +1 } },
-	{ MODKEY|Mod1Mask,              XK_l,      incrgaps,       {.i = -1 } },
-	{ MODKEY|Mod1Mask|ShiftMask,    XK_h,      incrogaps,      {.i = +1 } },
-	{ MODKEY|Mod1Mask|ShiftMask,    XK_l,      incrogaps,      {.i = -1 } },
-	{ MODKEY|Mod1Mask|ControlMask,  XK_h,      incrigaps,      {.i = +1 } },
-	{ MODKEY|Mod1Mask|ControlMask,  XK_l,      incrigaps,      {.i = -1 } },
-	{ MODKEY|Mod1Mask,              XK_0,      togglegaps,     {0} },
-	{ MODKEY|Mod1Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
+	{ MODKEY|Mod4Mask,              XK_h,      incrgaps,       {.i = +1 } },
+	{ MODKEY|Mod4Mask,              XK_l,      incrgaps,       {.i = -1 } },
+	{ MODKEY|Mod4Mask|ShiftMask,    XK_h,      incrogaps,      {.i = +1 } },
+	{ MODKEY|Mod4Mask|ShiftMask,    XK_l,      incrogaps,      {.i = -1 } },
+	{ MODKEY|Mod4Mask|ControlMask,  XK_h,      incrigaps,      {.i = +1 } },
+	{ MODKEY|Mod4Mask|ControlMask,  XK_l,      incrigaps,      {.i = -1 } },
+	{ MODKEY|Mod4Mask,              XK_0,      togglegaps,     {0} },
+	{ MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
 	{ MODKEY,                       XK_y,      incrihgaps,     {.i = +1 } },
 	{ MODKEY,                       XK_o,      incrihgaps,     {.i = -1 } },
 	{ MODKEY|ControlMask,           XK_y,      incrivgaps,     {.i = +1 } },
